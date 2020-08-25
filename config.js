900f2e4ecb8db5a8bd851422eb867742d6864f11
riki <rahulchrty@gmail.com>
2020-08-26 00:40:49 +0530

    tos and guest

--- config.js
+++ config.js
@@ -3,7 +3,9 @@ const config = {
     proxy: {
         rules: {
             '.*/test': 'http://localhost:9444/',
-            '.*/yoda': 'http://localhost:9444/cdn'
+            '.*/yoda': 'http://localhost:9444/cdn',
+            '.*/servlet/chatHttp/tos': 'http://localhost:9444/tosAccepted',
+            '.*/servlet/chatHttp/guestConnect': 'http://localhost:9444/guestConnect'
         }
     }
 };
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const express = require('express');
 const httpProxy = require('http-proxy');
 const HttpProxyRules = require('http-proxy-rules');
+const url = require('url');
 const config = require('./config');
 const APP_PORT = 9444;
 const PROXY_PORT = 9441;
@@ -13,13 +14,34 @@ const proxyRules = new HttpProxyRules(config.proxy);
 
 let proxy = httpProxy.createProxyServer({});
 
-let server = http.createServer(async function(req, res) {
+
+let server = http.createServer( async function (req, res) {
+  if (req.url.search('chatHttp') > -1) {
+      let query = (url.parse(req.url, true)).query;
+      if (query.data) {
+          let data = JSON.parse(query.data);
+          // Rewrite URL with new user informations
+          req.url = '/servlet/chatHttp?data=' + encodeURIComponent(JSON.stringify(data));
+
+          // If talk type is cguAccepted, redirect to Express API
+          if (data.type.localeCompare('tosAccept') === 0) {
+              req.url = '/servlet/chatHttp/tos';
+          }
+          // If talk type is guestConnect, redirect to Express API
+          if (data.type.localeCompare('guestConnect') === 0) {
+              req.url = '/servlet/chatHttp/guestConnect';
+          }
+      }
+  }   
   let targetUrl = proxyRules.match(req);
   proxy.web(req, res, { target: targetUrl, secure: false });
-})
+});
 
-server.timeout = 1200000;
-server.listen(PROXY_PORT);
+// let server = http.createServer(async function(req, res) {
+//   let targetUrl = proxyRules.match(req);
+//   proxy.web(req, res, { target: targetUrl, secure: false });
+// })
+
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -27,6 +49,15 @@ app.get('/', (req, res) => {
 
 app.use('/cdn', express.static('public'))
 
-app.listen(APP_PORT, HOST);
+app.post('/tosAccepted', async (req, res) => {
+  res.json({"accepted": true});
+});
 
+app.get('/guestConnect', (req, res) => {
+  res.json({"user": "riki"});
+});
+
+server.timeout = 1200000;
+server.listen(PROXY_PORT);
+app.listen(APP_PORT, HOST);
 console.log(`Running on http://${HOST}:${PROXY_PORT}`);
